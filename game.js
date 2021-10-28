91bd4d8a2a1d020e534b9960c914df080c7c89e3
Noxan <59417659+noxan-dev@users.noreply.github.com>
2021-10-28 18:30:44 -0400

    add mobile support?

--- game.js
+++ game.js
@@ -13,6 +13,14 @@ $(document).keydown(function(event) {
   }
 });
 
+$(document).tap(function(event) {
+  if (!started) {
+    $("#level-title").text("Level:" + level);
+    nextSequence();
+    started = true;
+  }
+});
+
 $(".btn").click(function() {
   var userChosenColor = $(this).attr("id");
   userClickedPattern.push(userChosenColor);
